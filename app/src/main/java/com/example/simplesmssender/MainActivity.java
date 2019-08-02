a2499ba452d2d9c6641b514c36d01c21f34c9156
Mayoogh <mayoogh1997@gmail.com>
2019-08-02 13:51:54 +0530

    Added broadcast status toast messages

--- app/src/main/java/com/example/simplesmssender/MainActivity.java
+++ app/src/main/java/com/example/simplesmssender/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.simplesmssender;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -17,10 +23,10 @@ public class MainActivity extends AppCompatActivity {
     Button send;
     EditText phnNumber, sms;
 
-//    private final String SENT = "SMS_SENT";
-//    private final String DELIVERED = "SMS_DELIVERED";
-//    PendingIntent sentPI, deliveredPI;
-//    BroadcastReceiver smsSentReceiver, smsDeliveredReceiver;
+    private final String SENT = "SMS_SENT";
+    private final String DELIVERED = "SMS_DELIVERED";
+    PendingIntent sentPI, deliveredPI;
+    BroadcastReceiver smsSentReceiver, smsDeliveredReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +36,11 @@ public class MainActivity extends AppCompatActivity {
         send = findViewById(R.id.btnSend);
         phnNumber = findViewById(R.id.editText_phone);
         sms = findViewById(R.id.editText_sms);
-    }
 
+        sentPI = PendingIntent.getBroadcast(MainActivity.this, 0, new Intent(SENT), 0);
+        deliveredPI = PendingIntent.getBroadcast(MainActivity.this, 0, new Intent(DELIVERED), 0);
+    }
+//Checking if permission to send sms is granted or not
     public void SendSms(View view) {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -42,9 +51,82 @@ public class MainActivity extends AppCompatActivity {
             String message = sms.getText().toString();
             String number = phnNumber.getText().toString();
             SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(number, null, message, null, null);
-            Toast.makeText(MainActivity.this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
+            smsManager.sendTextMessage(number, null, message, sentPI, deliveredPI);
+//            Toast.makeText(MainActivity.this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
         }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
 
+        unregisterReceiver(smsSentReceiver);
+        unregisterReceiver(smsDeliveredReceiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        smsSentReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(context, "SMS sent successfully!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    //Something went wrong and there's no way to tell what, why or how.
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(context, "Generic failure!", Toast.LENGTH_SHORT).show();
+                        break;
+                    //Your device simply has no cell reception. You're probably in the middle of
+                    //nowhere, somewhere inside, underground, or up in space.
+                    //Certainly away from any cell phone tower.
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(context, "No service!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    //Something went wrong in the SMS stack, while doing something with a protocol
+                    //description unit (PDU) (most likely putting it together for transmission).
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(context, "Null PDU!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    //You switched your device into airplane mode, which tells your device exactly
+                    //"turn all radios off" (cell, wifi, Bluetooth, NFC, ...).
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(context, "Turn off airplane mode!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+
+            }
+        };
+
+        smsDeliveredReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                switch(getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(context, "SMS delivered!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(context, "SMS not delivered!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+            }
+        };
+
+        //register the BroadCastReceivers to listen for a specific broadcast
+        //if they "hear" that broadcast, it will activate their onReceive() method
+        registerReceiver(smsSentReceiver, new IntentFilter(SENT));
+        registerReceiver(smsDeliveredReceiver, new IntentFilter(DELIVERED));
     }
 }
+
